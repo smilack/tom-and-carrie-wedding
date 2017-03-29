a5f8a5c2a57e1d2dee07fb4a37adffa2f499aaa0
CCrowRun <carrie.g.crow@gmail.com>
2017-03-29 16:06:55 -0500

    Closed object tag
    Added a curly brace for the Drinks object

--- app.js
+++ app.js
@@ -120,6 +120,6 @@ var madison = [{
 		picture: "images.jpg",
 		embed: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2914.404529500414!2d-89.39384404859314!3d43.074989979043025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88065368ff37b581%3A0xe22809618b8009f8!2sThe+Robin+Room!5e0!3m2!1sen!2sus!4v1490820517403" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
 		map: 'https://www.google.com/maps/place/The+Robin+Room/@43.07499,-89.393844,17z/data=!4m5!3m4!1s0x88065368ff37b581:0xe22809618b8009f8!8m2!3d43.0836191!4d-89.3771361'
-}];
+}]};
 
 })();
