c4226e6056176abafcdc20dad8acb5d6d3343c2f
smilack <tom@smilack.net>
2017-04-26 15:17:40 -0500

    Added a 'smooth' directive which scrolls smoothly to the target of a link

--- javascript/app.js
+++ javascript/app.js
@@ -54,11 +54,52 @@
  	};
  });
 
- app.controller('ScrollController', function($scope, $location, $anchorScroll) {
- 	var i = 1;
+ app.directive('smooth', function() {
+   return {
+      restrict: 'A',
+      controller: 'ScrollController'
+   };
+});
 
- 	$scope.items = [{ id: i, name: 'Page'}]
- });
+
+ app.controller('ScrollController', ['$window', '$element', function($window, $element) {
+   function setPosition(startTime, endTime, startPos, endPos) {
+      var now = Date.now();
+
+      if(now < endTime) {
+         var interval   = endTime - startTime,
+             timePassed = now     - startTime,
+             ratio      = timePassed / interval,
+             top        = (0.5 - Math.cos(ratio * Math.PI) / 2) * (endPos - startPos) + startPos; //default jquery easing function
+
+         $window.document.scrollingElement.scrollTop = top;
+
+         $window.requestAnimationFrame(setPosition.bind(null, startTime, endTime, startPos, endPos));
+
+      } else {
+         $window.document.scrollingElement.scrollTop = endPos;
+      }
+   }
+
+   $element.on('click', function() {
+
+      var startTime = Date.now(),
+          endTime   = startTime + 1000,
+          startPos  = $window.document.scrollingElement.scrollTop,
+          toId      = $element.attr('href').substring(1),
+          toElement;
+
+      if(toId == "") {
+         toElement = document.getElementById("home");
+      } else {
+         toElement = document.getElementById($element.attr('href').substring(1));
+      }
+
+      var endPos = startPos + toElement.getBoundingClientRect().top;
+         
+      $window.requestAnimationFrame(setPosition.bind(null, startTime, endTime, startPos, endPos));
+   });
+ }]);
 
  app.controller('StoryController', ['$window', '$timeout', function($window, $timeout) {
    $timeout(function() { angular.element($window).triggerHandler('resize'); }, 0);
