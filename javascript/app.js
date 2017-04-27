d9ebfd73ed4a7fba4ae24a3af153322fa56f4504
CCrowRun <carrie.g.crow@gmail.com>
2017-04-27 13:34:54 -0500

    app.js update

--- javascript/app.js
+++ javascript/app.js
@@ -70,32 +70,4 @@ app.controller('ActivityController', function(){
 	
 });
 
-//Controller manages viewing array of Gallery Photos
-app.controller('GalleryController', function($scope){
-	$scope.photos = gallery;
-
-	// initial image index
-	$scope._Index = 0;
-
-	// if a current image is the same as requested image
-	$scope.isActive = function (index) {
-		return $scope._Index === index;
-	};
-
-	// show prev image
-	$scope.showPrev = function () {
-		$scope._Index = ($scope._Index > 0) ? --$scope._Index : $scope.photos.length - 1;
-	};
-
-	// show next image
-	$scope.showNext = function () {
-		$scope._Index = ($scope._Index < $scope.photos.length - 1) ? ++$scope._Index : 0;
-	};
-
-	// show a certain image
-	$scope.showPhoto = function (index) {
-		$scope._Index = index;
-	};
-});
-
 })();
