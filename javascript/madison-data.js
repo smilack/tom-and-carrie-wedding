ab67316ce17953e73d25af3187cb6424122235d4
smilack <tom@smilack.net>
2018-04-26 12:01:31 -0500

    Updated 'Things to do in madison' section

--- javascript/madison-data.js
+++ javascript/madison-data.js
@@ -8,12 +8,6 @@ var madison = [{
 		picture: "images.jpg",
 		map: 'https://www.google.com/maps/place/Wisconsin+State+Capitol/@43.0747126,-89.3865672,17z/data=!3m1!4b1!4m5!3m4!1s0x880656a97df881e5:0x503974506f6ebae4!8m2!3d43.0747126!4d-89.3843732'
 	}, {
-		name: 'Old Fashioned',
-		description: 'Often cited as the best fried cheese curds in Madison, the Old Fashioned is a one stop place to find all the local treasures.',
-		link: 'http://www.theoldfashioned.com/',
-		picture: "images.jpg",
-		map: 'https://www.google.com/maps/place/The+Old+Fashioned/@43.0763038,-89.3858332,17z/data=!3m1!4b1!4m5!3m4!1s0x88065340a82a181f:0x18a416d89380b324!8m2!3d43.0763038!4d-89.3836392'
-	}, {
 		name: 'Wisconsin State Capitol',
 		description: 'The beautiful building sits right on the square, overlooking both lakes. Visitors can go to a lookout to see the lakes and cities. Hands down, best use of badger statues.',
 		link: 'http://tours.wisconsin.gov/',
@@ -28,6 +22,12 @@ var madison = [{
 		link: 'http://www.tipsycowmadison.com/',
 		picture: "images.jpg",
 		map: 'https://www.google.com/maps/place/Tipsy+Cow/@43.0747897,-89.3837106,17z/data=!3m1!4b1!4m5!3m4!1s0x8806533fa1794d05:0x8ed80142620cbd69!8m2!3d43.0747897!4d-89.3815166'
+	}, {
+		name: 'Old Fashioned',
+		description: 'Often cited as the best fried cheese curds in Madison, the Old Fashioned is a one stop place to find all the local treasures.',
+		link: 'http://www.theoldfashioned.com/',
+		picture: "images.jpg",
+		map: 'https://www.google.com/maps/place/The+Old+Fashioned/@43.0763038,-89.3858332,17z/data=!3m1!4b1!4m5!3m4!1s0x88065340a82a181f:0x18a416d89380b324!8m2!3d43.0763038!4d-89.3836392'
 	}]
 }, {
 	category: 'Drinks',
@@ -39,8 +39,23 @@ var madison = [{
 		map: 'https://www.google.com/maps/place/HopCat/@43.07499,-89.393844,17z/data=!3m1!4b1!4m5!3m4!1s0x88065336368fdd39:0xe512fb6de3e4776b!8m2!3d43.07499!4d-89.39165'
 	}, {
 		name: 'The Robin Room',
-		description: 'Hands down best use of badgers',
+		description: 'Just really good cocktails.',
 		link: 'http://www.robinroombar.com/',
 		picture: "images.jpg",
-		map: 'https://www.google.com/maps/place/The+Robin+Room/@43.07499,-89.393844,17z/data=!4m5!3m4!1s0x88065368ff37b581:0xe22809618b8009f8!8m2!3d43.0836191!4d-89.3771361'
+		map: 'https://goo.gl/maps/SUDv7FBSLap'
+   }]
+}, {
+	category: 'Outdoors',
+	items: [{
+		name: 'Devil\'s Lake State Park',
+		description: 'A gorgeous lake with several hiking trails - also where our engagement photos were taken!',
+		link: 'https://www.devilslakewisconsin.com/',
+		picture: "images.jpg",
+		map: 'https://goo.gl/maps/nVkFQgUe29T2'
+   }]
+}, {
+   category: "Other?",
+   items: [{
+      name: 'Let us know!',
+      description: 'Madison\'s got much more than a city this size has any right to. If you let us know what kind of food or drinks, or museums or attractions you like, we can recommend something just for you!',
 }]}];
